c27cb50c63bb5eac881e52796556b16b07a46fdd
kwongoum <avit.14@hotmail.com>
2021-04-12 00:02:04 +0200

    projet module done  with Spring Initializr

---
+++ immocam/src/main/java/be/wkam/immocam/ImmocamApplication.java
@@ -0,0 +1,13 @@
+package be.wkam.immocam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmocamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmocamApplication.class, args);
+    }
+
+}
---
+++ immocam/src/test/java/be/wkam/immocam/ImmocamApplicationTests.java
@@ -0,0 +1,13 @@
+package be.wkam.immocam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmocamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
